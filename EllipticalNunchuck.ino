22d6b34e4c99a0b233c94da084e5a962164b9fb1
arpruss <arpruss@gmail.com>
2017-10-01 22:09:38 -0500

    WARNINGS

--- EllipticalNunchuck.ino
+++ EllipticalNunchuck.ino
@@ -1,3 +1,16 @@
+/*
+ *
+ * This doesn't really work. :-(
+ * It ran for a bit on my digispark knockoff, and then failed spectacularly and smokily:
+ *   The digispark was burned out. And Windows warned about a surge on the USB port. I had to
+ *   reboot the computer to get the USB port working again.
+ * The code might be useful for someone, though. I plan to port it to stm32 and see if it works.
+ * I have a hunch that the problems had some connection to the fact that the Nunchuck is designed
+ * for 3.3V and the Digispark runs at 5V.
+ *
+ */
+
+
 //TODO: debounce direction switch
 
 #include <DigiJoystick.h>
